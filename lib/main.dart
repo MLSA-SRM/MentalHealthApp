e74e5ac421a761b3d60ba63d3be940e8f5050024
Vidit Misra <sachitmisra01@gmail.com>
2020-07-15 00:16:40 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,14 @@ void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(
-        MaterialApp(
-          title: 'Navigation Basics',
-          home: MyApp(),
-          debugShowCheckedModeBanner: false,
-        ),
+      MaterialApp(
+        title: 'Navigation Basics',
+        home: MyApp(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
-      });
+  });
 }
-
 
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
   if (message.containsKey('data')) {
@@ -58,26 +57,28 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   final int delayedAmount = 1000;
   SharedPreferences prefs;
   FirebaseMessaging firebaseMessaging = FirebaseMessaging();
-  bool onCallback = false,buttontoshow = false;
-  String url,_result;
+  bool onCallback = false, buttontoshow = false;
+  String url, _result;
 
   getresult() async {
     prefs = await SharedPreferences.getInstance();
     final _result = prefs.getString('status');
     print(_result);
-    if(_result != null)
-    buttontoshow = true;
+    if (_result != null) buttontoshow = true;
   }
 
-  checkDeviceId() async{
+  checkDeviceId() async {
     prefs = await SharedPreferences.getInstance();
     if (prefs.getString('deviceToken')?.isEmpty ?? true) {
       String deviceToken = await firebaseMessaging.getToken();
-      await FirebaseDatabase.instance.reference().child("fcm-token").child(deviceToken).set({"token": deviceToken});
+      await FirebaseDatabase.instance
+          .reference()
+          .child("fcm-token")
+          .child(deviceToken)
+          .set({"token": deviceToken});
       prefs.setString('deviceToken', deviceToken);
     }
   }
-
 
   @override
   void initState() {
@@ -90,28 +91,24 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
         print("onMessage: $message");
         print(message['notification']['body'] + "message");
         url = message['notification']['body'];
-        setState(() {
-
-        });
+        setState(() {});
       },
       onBackgroundMessage: myBackgroundMessageHandler,
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
         print(message['data']['body']);
         url = message['data']['body'];
-        Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewPage(url)));
-        setState(() {
-
-        });
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => WebViewPage(url)));
+        setState(() {});
       },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
         print(message['data']['body']);
         url = message['data']['body'];
-        Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewPage(url)));
-        setState(() {
-
-        });
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => WebViewPage(url)));
+        setState(() {});
       },
     );
   }
@@ -136,15 +133,12 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     //     .set({'chala kya?': "haan"});
     // print('done');
 
-    setState(() {
-      
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context,
-        allowFontScaling: true);
+    ScreenUtil.init(context, allowFontScaling: true);
 //     FirebaseDatabase.instance.reference().child('recent').child('id')
 // .set({
 // 'title': 'Realtime db rocks',
@@ -155,100 +149,109 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 20,
-                  ),
-                  ShowUp(
-                    child: AvatarGlow(
-                      //curve: Curves.easeOut,
-                      endRadius: _width/3.5,
-                      duration: Duration(seconds: 3),
-                      glowColor: Colors.blueAccent,
-                      repeat: true,
-                      child: GestureDetector(
-                        onTap: () async {
-                         //launch("https://docs.google.com/document/d/1-UaRguuQuFR4R08Q4jtmiL9KCL_B-ZwYG70tQImGNgU/edit?ts=5ecccadb");
-                        },
-                        child: Material(
-                            elevation: 8.0,
-                            shape: CircleBorder(),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.grey[100],
-                              child:
-                                  Image(image: AssetImage('assets/mainlogo.png')),
-                              radius: _width/5,
-                            )),
-                      ),
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 20,
                     ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 50,
-                  ),
-                  ShowUp(
-                    child: Text(
-                      "Here For You",
-                      style: TextStyle(
-                          fontSize: _width/15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    
-                  ),
-                  SizedBox(
-                    height: _height/70,
-                  ),
-                  ShowUp(
-                    child: Text(
-                      "&",
-                      style: TextStyle(fontSize: _width/15, color: Colors.black),
-                    ),
-                   
-                  ),
-                  SizedBox(
-                    height: _height/70,
-                  ),
-                  ShowUp(
-                    child: Text(
-                      "\'The thoughts in your head\'",
-                      style: TextStyle(fontSize: _width/18, color: Colors.black),
-                    ),
-                    
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 40,
-                  ),
-                      ShowUp(
-                        child: Text("Developed By",style: TextStyle(
-                          fontSize: _width/25
-                        ),),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 40,
-                      ),
-                      ShowUp(
-                   
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage("assets/mspc.png"),
-                          height: 250.h,
+                    ShowUp(
+                      child: AvatarGlow(
+                        //curve: Curves.easeOut,
+                        endRadius: _width / 3.5,
+                        duration: Duration(seconds: 3),
+                        glowColor: Colors.blueAccent,
+                        repeat: true,
+                        child: GestureDetector(
+                          onTap: () async {
+                            //launch("https://docs.google.com/document/d/1-UaRguuQuFR4R08Q4jtmiL9KCL_B-ZwYG70tQImGNgU/edit?ts=5ecccadb");
+                          },
+                          child: Material(
+                              elevation: 8.0,
+                              shape: CircleBorder(),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.grey[100],
+                                child: Image(
+                                    image: AssetImage('assets/mainlogo.png')),
+                                radius: _width / 5,
+                              )),
                         ),
-                        Image(
-                          image: AssetImage("assets/envision.png"),
-                          height: 250.h,
-                        ),
-                      ],
-                    )
-                    ,),]),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 50,
+                    ),
+                    ShowUp(
+                      child: Text(
+                        "Here For You",
+                        style: TextStyle(
+                            fontSize: _width / 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(
+                      height: _height / 70,
+                    ),
+                    ShowUp(
+                      child: Text(
+                        "&",
+                        style: TextStyle(
+                            fontSize: _width / 15, color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(
+                      height: _height / 70,
+                    ),
+                    ShowUp(
+                      child: Text(
+                        "\'The thoughts in your head\'",
+                        style: TextStyle(
+                            fontSize: _width / 18, color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 40,
+                    ),
+                    ShowUp(
+                      child: Text(
+                        "Developed By",
+                        style: TextStyle(fontSize: _width / 25),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 40,
+                    ),
+                    ShowUp(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () async {
+                              launch(
+                                  "https://msclubsrm.in/static/hereforyou.html");
+                            },
+                            child: Image(
+                              image: AssetImage("assets/mspc.png"),
+                              height: 250.h,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              launch("https://www.google.com/");
+                            },
+                            child: Image(
+                              image: AssetImage("assets/envision.png"),
+                              height: 250.h,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
                   Column(
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
@@ -257,16 +260,20 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                       ),
                       ShowUp(
                         child: Container(
-                          height: _height/17,
-                          width: _width/2,
+                          height: _height / 17,
+                          width: _width / 2,
                           child: RaisedButton(
                             //elevation: 10,
                             onPressed: () {
-                              !buttontoshow ? Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Onboarding()))
-                              : Navigator.push(context, MaterialPageRoute(builder: (context) => Quiz()));
+                              !buttontoshow
+                                  ? Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Onboarding()))
+                                  : Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Quiz()));
                             },
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.all(
@@ -278,19 +285,18 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                             padding: const EdgeInsets.all(8.0),
                             child: new Text(
                               !buttontoshow ? "Get Started" : "Retake Quiz",
-                              style: TextStyle(fontSize: _width/23),
+                              style: TextStyle(fontSize: _width / 23),
                             ),
                           ),
                         ),
-                        
                       ),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height/70,
+                        height: MediaQuery.of(context).size.height / 70,
                       ),
                       ShowUp(
                         child: Container(
-                          height: _height/17,
-                          width: _width/2,
+                          height: _height / 17,
+                          width: _width / 2,
                           child: RaisedButton(
                             //    elevation: 10,
                             onPressed: () {
@@ -309,23 +315,23 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                             padding: const EdgeInsets.all(8.0),
                             child: new Text(
                               "Articles",
-                              style: TextStyle(fontSize: _width/23),
+                              style: TextStyle(fontSize: _width / 23),
                             ),
                           ),
                         ),
-                        
                       ),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height/70,
+                        height: MediaQuery.of(context).size.height / 70,
                       ),
                       ShowUp(
                         child: Container(
-                          height: _height/17,
-                          width: _width/2,
+                          height: _height / 17,
+                          width: _width / 2,
                           child: RaisedButton(
                             //elevation: 10,
-                            onPressed: () async{
-                              launch("https://github.com/MSPC-Tech/MentalHealthApp/raw/master/static/Get%20Help%20Now.pdf");
+                            onPressed: () async {
+                              launch(
+                                  "https://github.com/MSPC-Tech/MentalHealthApp/raw/master/static/Get%20Help%20Now.pdf");
                             },
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.all(
@@ -337,20 +343,18 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                             padding: const EdgeInsets.all(8.0),
                             child: new Text(
                               "Get Help Now!",
-                              style: TextStyle(fontSize: _width/23),
+                              style: TextStyle(fontSize: _width / 23),
                             ),
                           ),
                         ),
-                        
                       ),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height/50,
+                        height: MediaQuery.of(context).size.height / 50,
                       ),
                     ],
                   ),
-              ]),
-        )
-      ),
+                ]),
+          )),
     );
   }
 }
